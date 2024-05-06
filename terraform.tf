07ffeb44ace5e8a771edd2d936ba0956cf9ead85
Chandra-mohan31 <chandrashanmugam007@gmail.com>
2024-05-06 17:59:07 +0530

    update terraform code to use backend args from cli

--- terraform.tf
+++ terraform.tf
@@ -2,11 +2,11 @@ terraform {
   required_version = ">= 1.6.0"
   backend "s3" {
     # key    = "terraform.tfstate"
-    # region = "us-east-1"
+    region = "us-east-1"
     # bucket = "sample-pipeline-poc-cm"
 
     # dynamodb_table = "tf-state-lock-dynamodb-cm"
-    encrypt        = true
+    encrypt = true
   }
   required_providers {
     aws = {
