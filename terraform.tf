4400808eb65a141a6eee390c0fe5577c3600a48e
Chandra-mohan31 <chandrashanmugam007@gmail.com>
2024-05-06 17:16:53 +0530

    configure the backed from terraform cli

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,10 @@
 terraform {
   required_version = ">= 1.6.0"
   backend "s3" {
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    bucket         = "sample-poc-pipeline-dev"
-    dynamodb_table = "sample-pipeline-poc-lock-table-dev"
+    # key            = "terraform.tfstate"
+    # region         = "us-east-1"
+    # bucket         = "sample-poc-pipeline-dev"
+    # dynamodb_table = "sample-pipeline-poc-lock-table-dev"
     encrypt        = true
   }
   required_providers {
